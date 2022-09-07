75bb64e5afdeecffedd8b0f04ee9fe3c3c2cabe0
saharaliabdou <sahar.aliabdou@gmail.com>
2022-09-07 09:03:17 +0200

    tried to add button to image slide

--- script/carouselView.js
+++ script/carouselView.js
@@ -53,33 +53,77 @@ function renderImages(src, tag, imgTitle, imgComment) {
    // a.href = img.src;
    // })
 
-const lightbox = document.createElement('div')
-lightbox.id = 'lightbox'
-document.body.appendChild(lightbox)
+   const lightbox = document.createElement('div')
+   lightbox.id = 'lightbox'
+   document.body.appendChild(lightbox)
+   
+   const carouselactions = document.createElement('div');
+   carouselactions.className= 'carousel-actions';
+   lightbox.appendChild(carouselactions);
+
+   const buttonprev = document.createElement('button');
+   buttonprev.id ='carousel-button-prev';
+   carouselactions.appendChild(buttonprev);
+
+   
+   
+
+
+   const images = document.querySelectorAll('img')
+   images.forEach(image => {
+     image.addEventListener('click', e => {
+       lightbox.classList.add('active')
+       const img = document.createElement('img')
+       img.src = image.src
+       
+       
+       while (lightbox.firstChild) {
+         lightbox.removeChild(lightbox.firstChild)
+        
+       
+       }
+       lightbox.appendChild(img)
+       
+       
+       
+     })
+   })
+   
+   lightbox.addEventListener('click', e => {
+       if (e.target !== e.currentTarget) return
+       lightbox.classList.remove('active')
+     })
+   };
+   
+   nextButton.addEventListener("click", plusSlides(1));
+   prevButton.addEventListener("click", plusSlides(-1));
 
 
 
-const images = document.querySelectorAll('img')
-images.forEach(image => {
-  image.addEventListener('click', e => {
-    lightbox.classList.add('active')
-    const img = document.createElement('img')
-    img.src = image.src
-    while (lightbox.firstChild) {
-      lightbox.removeChild(lightbox.firstChild)
-    }
-    lightbox.appendChild(img)
-    
-    
-  })
-})
-
-lightbox.addEventListener('click', e => {
-  if (e.target !== e.currentTarget) return
-  lightbox.classList.remove('active')
-})
 
 
-};
+const nextButton = document.getElementById("carousel-button-next");
+const prevButton = document.getElementById("carousel-button-prev");
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
 
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("active");
+  
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  
+  slides[slideIndex-1].style.display = "block";
+  
+}
