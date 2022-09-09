be8d804b99c1f96e5cfa5d972c044a51714fd259
saharaliabdou <sahar.aliabdou@gmail.com>
2022-09-09 16:31:14 +0200

    Merge branch 'master' of https://github.com/jagharloggatin/WebbProjekt

--- script/carouselView.js
+++ script/carouselView.js
@@ -53,47 +53,12 @@ function renderImages(src, tag, imgTitle, imgComment) {
    // a.href = img.src;
    // })
 
-   const lightbox = document.createElement('div')
-   lightbox.id = 'lightbox'
-   document.body.appendChild(lightbox)
-   
-   
-
-   
-   
+const lightbox = document.createElement('div')
+lightbox.id = 'lightbox'
+document.body.appendChild(lightbox)
 
 
-   const images = document.querySelectorAll('img')
-   images.forEach(image => {
-     image.addEventListener('click', e => {
-       lightbox.classList.add('active')
-       const img = document.createElement('img')
-       img.src = image.src
-       
-       
-       while (lightbox.firstChild) {
-         lightbox.removeChild(lightbox.firstChild)
-        
-       
-       }
-       lightbox.appendChild(img)
-       
-       
-       
-     })
-   })
-   
-   lightbox.addEventListener('click', e => {
-       if (e.target !== e.currentTarget) return
-       lightbox.classList.remove('active')
-     })
-   };
-   
-   
 
-
-<<<<<<< HEAD
-=======
 const images = document.querySelectorAll('img')
 images.forEach(image => {
   image.addEventListener('click', e => {
@@ -111,6 +76,6 @@ lightbox.addEventListener('click', e => {
   if (e.target !== e.currentTarget) return
   lightbox.classList.remove('active')
 })
->>>>>>> f7393cdb774e88631b44c53f02a2cea5c9b09e7d
 
 
+};
