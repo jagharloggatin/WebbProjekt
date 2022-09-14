7f60d0e4be251a1478df6e65b0963dd00ada7f34
Jonathan <89255112+jagharloggatin@users.noreply.github.com>
2022-09-14 15:28:09 +0200

    rating

--- script/appDisplayAlbumImages.js
+++ script/appDisplayAlbumImages.js
@@ -15,6 +15,7 @@ window.addEventListener('DOMContentLoaded', async () => {
                 renderImages(`${album.path}/${picture.imgLoRes}`, picture.id, picture.title, picture.comment);
             }
         }
+
     }
     // for (const album of library.albums) {
     //     if (album.id === savedAlbumId) {
@@ -30,6 +31,8 @@ function renderImages(src, tag, imgTitle, imgComment) {
 
     const container = document.createElement('div');
     container.id = 'imgAndCommentContainer';
+
+
 
     const div = document.createElement('div');
     div.className = `image-item`;
@@ -55,6 +58,40 @@ function renderImages(src, tag, imgTitle, imgComment) {
     imgCommentDiv.textContent = `${imgComment}`;
     container.appendChild(imgCommentDiv);
 
+    const ratingwrap = document.createElement('div');
+    ratingwrap.className = "rating-wrap"
+
+    const center = document.createElement('div');
+    center.className = "center"
+    ratingwrap.appendChild(center);
+
+    const rating = document.createElement('fieldset');
+    rating.className = "rating";
+    center.appendChild(rating);
+
+    const input05 = document.createElement('input'); input05.type= "radio"; input05.id="star0.5"; input05.name="rating"; input05.value="0";
+    const input1 = document.createElement('input'); input1.type= "radio"; input1.id="star1"; input1.name="rating"; input1.value="1";
+    const input15 = document.createElement('input'); input15.type= "radio"; input15.id="star1.5"; input15.name="rating"; input1.value="1.5";
+    const input2 = document.createElement('input'); input2.type= "radio"; input2.id="star2"; input2.name="rating"; input2.value="2";
+    const input25 = document.createElement('input'); input25.type= "radio"; input25.id="star2.5"; input25.name="rating"; input1.value="2.5";
+    const input3 = document.createElement('input'); input3.type= "radio"; input3.id="star3"; input3.name="rating"; input3.value="3";
+    const input35 = document.createElement('input'); input35.type= "radio"; input35.id="star3.5"; input35.name="rating"; input3.value="3.5";
+    const input4 = document.createElement('input'); input4.type= "radio"; input4.id="star4"; input4.name="rating"; input4.value="4";
+    const input45 = document.createElement('input'); input45.type= "radio"; input45.id="star4.5"; input45.name="rating"; input45.value="4.5";
+    const input5 = document.createElement('input'); input5.type= "radio"; input5.id="star5"; input5.name="rating"; input5.value="5";
+
+    rating.appendChild(input1);
+    rating.appendChild(input15);
+    rating.appendChild(input2);
+    rating.appendChild(input25);
+    rating.appendChild(input3);
+    rating.appendChild(input35);
+    rating.appendChild(input4);
+    rating.appendChild(input45);
+    rating.appendChild(input5);
+
+    container.appendChild(ratingwrap);
+
     const imgFlex = document.querySelector('.image-wrap');
     imgFlex.appendChild(container);
 
@@ -62,22 +99,21 @@ function renderImages(src, tag, imgTitle, imgComment) {
     lightbox.id = 'lightbox';
     window.document.body.appendChild(lightbox)
 
-<<<<<<< HEAD
     // --------------------------------Testar rating----------------------------------
 
-    const starRating = document.createElement("div")
-    starRating.className = "rating";
-    container.appendChild(starRating);
-
-    let star = document.querySelectorAll('input');
-    let showValue = document.querySelector('#rating-value');
-
-    for(let i = 0; i < star.length; i++) {
-    star[i].addEventListener('click', function(){
-    i = this.value;
-    showValue.innerHTML = i + " out of 5";
-    });
-    }
+    // const starRating = document.createElement("div")
+    // starRating.className = "rating";
+    // container.appendChild(starRating);
+    //
+    // let star = document.querySelectorAll('input');
+    // let showValue = document.querySelector('#rating-value');
+    //
+    // for(let i = 0; i < star.length; i++) {
+    // star[i].addEventListener('click', function(){
+    // i = this.value;
+    // showValue.innerHTML = i + " out of 5";
+    // });
+    // }
     
     // function getRatings()
 
@@ -85,8 +121,8 @@ function renderImages(src, tag, imgTitle, imgComment) {
 
 
     //
-=======
->>>>>>> bfccfc9b41bd2821edc642ca0d1ad761b7fccd90
+// =======
+// >>>>>>> bfccfc9b41bd2821edc642ca0d1ad761b7fccd90
     // const lightbox = document.createElement('div')
     // lightbox.id = 'lightbox'
     // document.body.appendChild(lightbox)
